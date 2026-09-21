19dba6fd965cb75cf1b1c1c83552ccf5f703ff74
harryjbedford25 <harryjbedford25@gmail.com>
2026-09-21 21:57:50 +0100

    Fix hero image path: use correct case for Mockup.png

--- Hero.jsx
+++ Hero.jsx
@@ -50,7 +50,7 @@ export default function Hero() {
           </div>
 
           <div className="flex lg:justify-end">
-            <img src="/Screenshots/mockup.png" alt="RaceSense mockup" className="w-[80vw] max-w-[60rem] rounded-lg" />
+            <img src="/Screenshots/Mockup.png" alt="RaceSense mockup" className="w-[80vw] max-w-[60rem] rounded-lg" />
           </div>
         </div>
       </div>
